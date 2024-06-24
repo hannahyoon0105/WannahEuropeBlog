57d5197b3bd64a593063623b8ba9d6c4ff052c03
Wesley Allen <weal2937@colorado.edu>
2024-06-24 13:12:40 -0600

    finalizing database

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,45 +1,41 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL,
-    profile_pic VARCHAR(200)
+    password VARCHAR(60) NOT NULL
+    -- you guys had password as char and idk why
 );
-DROP TABLE IF EXISTS recipes;
-CREATE TABLE recipes (
-    recipe_id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    author VARCHAR(50) NOT NULL REFERENCES users(username),
-    body TEXT NOT NULL,
-    date_created TIMESTAMP NOT NULL
+
+DROP TABLE IF EXISTS bingo;
+CREATE TABLE bingo (
+    item_id SERIAL PRIMARY KEY,
+    body VARCHAR(100),
+    completed BOOLEAN NOT NULL
 );
+
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
-    author VARCHAR(50) NOT NULL REFERENCES users(username),
     caption VARCHAR(200),
-    recipe_id INTEGER NOT NULL REFERENCES recipes(recipe_id),
-    date_created TIMESTAMP NOT NULL, 
-    image_url VARCHAR(2000) NOT NULL,
-    original_flag BOOLEAN NOT NULL
+    date_created TIMESTAMP WITH TIME ZONE NOT NULL,
+    bingo_id INTEGER NOT NULL REFERENCES bingo(item_id),
+    image_filepath VARCHAR(2000) NOT NULL
 );
 
-DROP TABLE IF EXISTS followers;
-CREATE TABLE followers (
-    follower VARCHAR(50) NOT NULL REFERENCES users(username),
-    followee VARCHAR(50) NOT NULL REFERENCES users(username),
-    PRIMARY KEY (follower, followee)
-);
 DROP TABLE IF EXISTS likes;
 CREATE TABLE likes (
     post_id INTEGER NOT NULL REFERENCES posts(post_id),
     username VARCHAR(50) NOT NULL REFERENCES users(username),
     PRIMARY KEY (post_id, username)
 );
+
 DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     post_id INTEGER NOT NULL REFERENCES posts(post_id),
     username VARCHAR(50) NOT NULL REFERENCES users(username),
     body TEXT NOT NULL,
-    date_created TIMESTAMP NOT NULL
+    date_created TIMESTAMP WITH TIME ZONE NOT NULL
 );
+
+
+
